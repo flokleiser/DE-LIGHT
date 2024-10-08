a54435600d5b89fc614367d181c7e8e41cea44ef
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-10-08 20:49:20 +0200

    multiple keys able to be pressed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let circleColor;
 let previousKey = null;
+let keyStates = {};
 
 function setup() {
   createCanvas(1000, 800);
@@ -17,31 +18,56 @@ function handleKeyPress() {
   let currentKey = null;
 
   // handling key presses
-    if (keyIsDown(65)) { // A (weak)
+    // if (keyIsDown(65)) { // A (weak)
+    //   currentKey = 'a';
+    //   circleColor = color(0, 255, 0);
+    // } else if (keyIsDown(83)) { // S (mid)
+    //   currentKey = 's';
+    //   circleColor = color(0, 0, 255);
+    // } else if (keyIsDown(87)) { // W (strong)
+    //   currentKey = 'w';
+    //   circleColor = color(255, 0, 0);
+    // } else {
+    //   circleColor = color(255);
+    // }
+    if (keyStates[65]) { // A (weak)
       currentKey = 'a';
       circleColor = color(0, 255, 0);
-    } else if (keyIsDown(83)) { // S (mid)
+    } 
+    if (keyStates[83]) { // S (mid)
       currentKey = 's';
       circleColor = color(0, 0, 255);
-    } else if (keyIsDown(87)) { // W (strong)
+    } 
+    if (keyStates[87]) { // W (strong)
       currentKey = 'w';
       circleColor = color(255, 0, 0);
-    } else {
+    }
+    if (!keyStates[65] && !keyStates[83] && !keyStates[87]) {
       circleColor = color(255);
     }
+   
 
     // printing key presses to the console only once
     if (currentKey !== previousKey) {
       if (currentKey === 'a') {
-        console.log('a, weak punch')
+        console.log('A --> weak punch')
       } else if (currentKey === 's') {
-        console.log('s, mid punch')
+        console.log('S --> mid punch')
       } else if (currentKey === 'w') {
-        console.log('w, strong punch')
+        console.log('W --> strong punch')
       } else {
         console.log('-')
       }
       previousKey = currentKey;
     }
 
+  }
+
+  // checking if multiple keys pressed
+  function keyPressed() {
+    keyStates[keyCode] = true;
+  }
+  
+  function keyReleased() {
+    keyStates[keyCode] = false;
   }
