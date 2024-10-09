db5f09099c9a81ad1bb64d92aee49064ddb00c10
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-10-09 10:50:15 +0200

    updates wednesday morning

--- script.js
+++ script.js
@@ -1,46 +1,74 @@
 let circleColor;
+let targetColor
+let circleRadius1 = 250;
+let targetRadius = 250;
+let lerpAmount = 0.5;
 let previousKey = null;
 let keyStates = {};
 
-//todo: mode select with num keys
-let currentMode = 0;
-
+let currentMode = 1;
 
 // Initializing the canvas
 function setup() {
   createCanvas(1000, 800);
   circleColor = color(255);
+  targetColor = color(255)
 }
 
 // draw loop (runs every frame)
 function draw() {
+
+  // setMode()
+
   handleKeyPress()
   background(0);
+  circleColor = lerpColor(circleColor, targetColor, lerpAmount);
+  circleRadius1 = lerp(circleRadius1, targetRadius, lerpAmount);
   fill(circleColor);
-  ellipse(width / 2, height / 2, 100, 100);
+  ellipse(width / 2, height / 2, circleRadius1, circleRadius1);
+
+  textAlign(CENTER);
+  textSize(16);
+  text(`${key} : ${keyCode}`, 50, 50);
 }
 
 function handleKeyPress() {
   // handle the key presses
   let currentKey = null;
+
+  if (keyStates[49]) {
+    currentMode = 1;
+    console.log('Mode 1 selected');
+  } else if (keyStates[50]) {
+    currentMode = 2;
+    console.log('Mode 2 selected');
+  }
+
+  if (currentMode === 1) { // first mode
     if (keyStates[65]) { // A (weak)
       currentKey = 'a';
-      circleColor = color(0, 255, 0);
+      targetColor = color(238, 210, 2);
+      targetRadius = 275;
     } 
     if (keyStates[83]) { // S (mid)
       currentKey = 's';
-      circleColor = color(0, 0, 255);
+      targetColor = color(255, 102, 0);
+      targetRadius = 325;
     } 
     if (keyStates[87]) { // W (strong)
       currentKey = 'w';
-      circleColor = color(255, 0, 0);
+      targetColor = color(255, 0, 0);
+      targetRadius = 375;
     }
-    if (!keyStates[65] && !keyStates[83] && !keyStates[87]) {
-      circleColor = color(255);
+    if (!keyStates[65] && !keyStates[83] && !keyStates[87]) { //set to white otherwise
+      targetColor = color(255);
+      targetRadius = 250;
     }
-   
+  } else if (currentMode === 2) {
+    // testing different mode
+    console.log('mode 2, nothing works')
+  }
 
-    // printing key presses to the console only once
     if (currentKey !== previousKey) {
       if (currentKey === 'a') {
         console.log('A --> weak punch')
@@ -56,7 +84,7 @@ function handleKeyPress() {
 
   }
 
-  // checking if multiple keys pressed
+
   function keyPressed() {
     keyStates[keyCode] = true;
   }
